19ed51f956404fefc7ab6964e1374bd40c7ba673
bobo <sophia09930236@gmail.com>
2019-06-08 01:20:45 +0800

    add

--- homeworks/week3/hw3.js
+++ homeworks/week3/hw3.js
@@ -1,5 +1,19 @@
 function isPrime(n) {
-  console.log(n);
+  let result = [];
+  if (n <= 1) {
+    return false;
+  } else {
+    for (let i = 1; i <= n; i++) {
+      if (n % i === 0) {
+        result.push(i);
+      }
+    }
+    if (result.length > 2) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
 
 module.exports = isPrime;
