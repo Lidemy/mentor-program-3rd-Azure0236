fc711ef86cf017843187dfc3be5dbcafdbaf026d
bobo <sophia09930236@gmail.com>
2019-06-08 02:16:09 +0800

    revise2

--- homeworks/week3/hw2.test.js
+++ homeworks/week3/hw2.test.js
@@ -4,4 +4,13 @@ describe('hw2', () => {
   it('should return correct answer when str = nick', () => {
     expect(alphaSwap('nick')).toBe('NICK');
   });
+  it('should return correct answer when str = aBcd', () => {
+    expect(alphaSwap('aBcd')).toBe('AbCD');
+  });
+  it('should return correct answer when str =""', () => {
+    expect(alphaSwap('""')).toBe('""');
+  });
+  it('should return correct answer when str =123', () => {
+    expect(alphaSwap('123')).toBe('123');
+  });
 });
--- homeworks/week3/hw3.test.js
+++ homeworks/week3/hw3.test.js
@@ -4,4 +4,10 @@ describe('hw3', () => {
   it('should return correct answer when n = 1', () => {
     expect(isPrime(1)).toBe(false);
   });
+  it('should return correct answer when n = 2', () => {
+    expect(isPrime(2)).toBe(true);
+  });
+  it('should return correct answer when n = 40', () => {
+    expect(isPrime(40)).toBe(false);
+  });
 });
