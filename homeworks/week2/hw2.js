8c3ccd7e836644c88faf5d77c18edcaeb70cfe07
bobo <sophia09930236@gmail.com>
2019-06-08 03:58:06 +0800

    week2-hw

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,6 +1,6 @@
 function capitalize(str) {
-  let result = ""
-  return result + str[0].toUpperCase() + str.slice(1)
+  const result = '';
+  return result + str[0].toUpperCase() + str.slice(1);
 }
 
 console.log(capitalize('hello'));
